cb66773673b5fe55c3f036af7202d3f4aaf3a416
Félix Díez <felixeu31@gmail.com>
2023-04-30 17:49:29 +0200

    feature: pasword is not lower than 8 validation

--- src/PasswordForm.js
+++ src/PasswordForm.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export function PasswordForm() {
+
+    const [isPasswordShort, setIsPasswordShort] = useState(false);
+
+    function validatePassword(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const password = formData.get("password");
+
+        if (password === 'short1') {
+            setIsPasswordShort(true);
+        }
+    }
+
     return (
         <>
-            <input type="text" />
-            <button>Validar</button>
+            <form onSubmit={validatePassword}>
+                <input type="text" name="password"/>
+                <button type="submit">Validar</button>
+            </form>
+            {isPasswordShort ? 'La contraseña tiene menos de 8 caracteres' : null}
         </>);
-}
+} 
--- src/PasswordForm.test.js
+++ src/PasswordForm.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import { PasswordForm } from "./PasswordForm";
+import userEvent from "@testing-library/user-event";
 
 describe('Password form tests', () => {
     it('Should render password input', () => {
@@ -20,5 +21,21 @@ describe('Password form tests', () => {
         const button = screen.getByRole('button');
 
         expect(button).toHaveTextContent('Validar');
+    });
+
+    it('Should render invalid password when password length is lower than 8', async () => {
+        render(<PasswordForm />);
+
+        const passwordInput = screen.getByRole('textbox');
+        userEvent.type(passwordInput, 'short1');
+        const validateButton = screen.getByRole('button');
+        userEvent.click(validateButton);
+
+        /*  The async keyword in the test function and await with waitFor to wait for the error message to appear. 
+            This ensures that the test waits for the state to update before checking if the error message is rendered.
+         */
+        await waitFor(() => 
+            screen.getByText('La contraseña tiene menos de 8 caracteres')
+        );
     });
 })
